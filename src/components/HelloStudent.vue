5df60725f0febda2462095774abc19b5c3b1a88a
ymjoz <rossiblack@duck.com>
2024-10-22 12:10:18 +0800

    chore: some stuff

---
+++ src/components/HelloStudent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>
+      hi, {{ msg }}
+    </h1>
+    <h2>
+      Location: {{ location_song }}
+    </h2>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  defineProps({
+    msg: {
+      type: String,
+      default: "rossi.black"
+    }
+  })
+
+  const location_song = ref("yilan city")
+
+</script>
+
+<style scoped></style>
